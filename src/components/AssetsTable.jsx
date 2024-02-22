6d1fc81e4609e03dd17eb734941661502ea43f35
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2024-02-22 13:53:36 +0300

    The basic functionality of the application is ready
    The basic functionality of the application is ready

--- src/components/AssetsTable.jsx
+++ src/components/AssetsTable.jsx
@@ -1,10 +1,46 @@
+import { Table } from 'antd';
 import React from 'react';
+import { useCrypto } from '../context/cryptoContext';
+
+const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ['descend'],
+    },
+    {
+      title: 'Price, $',
+      dataIndex: 'price',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.amount - b.amount,
+    },
+  ];
+
 
 const AssetsTable = () => {
+
+    const { assets } = useCrypto()
+
+    const data = assets.map(asset => ({
+        key: asset.id,
+        name: asset.name,
+        price: asset.price,
+        amount: asset.amount,
+    }))
+
     return (
-        <div>
-            222
-        </div>
+        <Table 
+        columns={columns} 
+        dataSource={data} 
+        pagination={false}
+        />
     );
 };
 
